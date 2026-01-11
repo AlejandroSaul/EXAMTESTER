1977f51b3fdfbe97f2330ab15caa63ce63d3a68d
Alejandro Saul <saul_ba_ve_82@hotmail.com>
2026-01-11 01:30:45 -0600

    ExamTester V 0.2.3

--- examtester.app/frontendexamtester/src/App.js
+++ examtester.app/frontendexamtester/src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import InsertarPregunta from "./Preguntas/InsertarPregunta";
 import Navegacion from "./Preguntas/Navegacion";
+import Test from "./Preguntas/Test";
 
 function App() {
   return (
-    <div className="container text-center">
-      <h3>Exam Tester</h3>
-      <Navegacion/>
-    </div>
+    <BrowserRouter>
+      <div className="container text-center">
+        <h3>Exam Tester</h3>
+        <Navegacion />
+        <Routes>
+          <Route path="/agregar-pregunta"  element={<InsertarPregunta />} />
+          <Route path="/ir-a-test"  element={<Test />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
